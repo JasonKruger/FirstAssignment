25b716dd9a3b867bf30c3eecc5ba160fd340b556
Jason Kruger <jasonkruger90@gmail.com>
2022-07-20 02:28:50 +0200

    Worked on main file, and added Planning folder

--- Main.java
+++ Main.java
@@ -1,8 +1,35 @@
-import javax.sound.sampled.SourceDataLine;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-    }
+        Scanner myObj = new Scanner(System.in);
+        int PIN = 1234;
+        
+        System.out.println("Please enter your PIN");
+        int entry = myObj.nextInt();
 
+        while (entry != PIN)
+        {
+                System.out.println("Incorrect PIN. Please try again.");
+                System.out.print("Please enter your PIN: ");
+                entry = myObj.nextInt();
+            
+        }
+        System.out.println("PIN Accepted.");
+        
+        //FIRST STEP COMPLETE. NOW STEP 2
+
+        double Balance = 50;
+
+        System.out.println("Your balance is: " + Balance);
+        System.out.println("You may Withdraw or Deposit");
+
+        String="Choice"
+
+
+
+        
+    }
+    
 }
